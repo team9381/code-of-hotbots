5f4f5d02015f7e72a8f654897010a6cec5483662
Ethan Thames <ethanthames.mail@gmail.com>
2017-08-25 20:56:48 -0500

    Update BasicDrive.js

--- BasicDrive.js
+++ BasicDrive.js
@@ -2,17 +2,20 @@ var sdk, controller;
 alicorn.shard("BasicDrive")
     .create(function(self) {
         sdk = self.require("AndroidFTC"); // Get the library for interfacing with FTC parts
-        controller = sdk.get("gamepad");  // Create an object for the gamepad
+        controller = sdk.get("gamepad"); // Create an object for the gamepad
     })
     .update(function(self) {
-        var x = controller.get("x2");     // Get the controller stick's X axis
-        var y = controller.get("y1");     // Get the controller stick's Y axis
+        // Get the value of each axis of the joystick on the controller
+        var x = controller.get("x2");
+        var y = controller.get("y1");
         
-        var left = (x - y);               // Get the power of the motors on the left side of the robot
-        var right = (x + y);              // Get the power of the motors on the right side of the robot
+        // Get the power of the motors on the left side of the robot
+        var left = (x - y);
+        var right = (x + y);
         
-        sdk.set("leftFront", left);       // Set the motor named "leftFront"'s power to the variable left
-        sdk.set("leftBack", left);        // Set the motor named "leftBack"'s power to the variable left
-        sdk.set("rightFront", right);     // Set the motor named "rightFront"'s power to the variable right
-        sdk.set("rightBack", right);      // Set the motor named "rightBack"'s power to the variable right
+        // Set the motor power to the controller's axis
+        sdk.set("leftFront", left); 
+        sdk.set("leftBack", left); 
+        sdk.set("rightFront", right); 
+        sdk.set("rightBack", right); 
     });
